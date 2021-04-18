34c2c9f992083fa929fd4680e734c1ce190e4944
Duziest <39634650+Duziest@users.noreply.github.com>
2021-04-18 12:21:23 -0400

    Update ServerSelector.java

--- com/primalgamesllc/hqhub/guis/ServerSelector.java
+++ com/primalgamesllc/hqhub/guis/ServerSelector.java
@@ -192,9 +192,3 @@
 /*     */     } 
 /*     */   }
 /*     */ }
-
-
-/* Location:              /Users/benjaminmeyer/Downloads/HQHub.jar!/com/primalgamesllc/hqhub/guis/ServerSelector.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
