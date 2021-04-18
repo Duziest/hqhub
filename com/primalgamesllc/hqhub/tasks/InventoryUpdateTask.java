ac53aee552eda35655bed3596f8d57350e41eab1
Duziest <39634650+Duziest@users.noreply.github.com>
2021-04-18 12:17:10 -0400

    Update InventoryUpdateTask.java

--- com/primalgamesllc/hqhub/tasks/InventoryUpdateTask.java
+++ com/primalgamesllc/hqhub/tasks/InventoryUpdateTask.java
@@ -97,8 +97,3 @@
 /*    */   }
 /*    */ }
 
-
-/* Location:              /Users/benjaminmeyer/Downloads/HQHub.jar!/com/primalgamesllc/hqhub/tasks/InventoryUpdateTask.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
