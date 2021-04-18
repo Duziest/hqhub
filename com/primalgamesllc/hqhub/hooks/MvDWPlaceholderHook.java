74b7d7cf37a807e903ef2cc00e48860a5c252ec0
Duziest <39634650+Duziest@users.noreply.github.com>
2021-04-18 12:18:23 -0400

    Update MvDWPlaceholderHook.java

--- com/primalgamesllc/hqhub/hooks/MvDWPlaceholderHook.java
+++ com/primalgamesllc/hqhub/hooks/MvDWPlaceholderHook.java
@@ -31,8 +31,3 @@
 /*    */   }
 /*    */ }
 
-
-/* Location:              /Users/benjaminmeyer/Downloads/HQHub.jar!/com/primalgamesllc/hqhub/hooks/MvDWPlaceholderHook.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
