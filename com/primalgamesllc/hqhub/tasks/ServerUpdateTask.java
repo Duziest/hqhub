028ec68499b818f4ea94302f835e2b6a904ac6f5
Duziest <39634650+Duziest@users.noreply.github.com>
2021-04-18 12:17:20 -0400

    Update ServerUpdateTask.java

--- com/primalgamesllc/hqhub/tasks/ServerUpdateTask.java
+++ com/primalgamesllc/hqhub/tasks/ServerUpdateTask.java
@@ -41,9 +41,3 @@
 /*    */     } 
 /*    */   }
 /*    */ }
-
-
-/* Location:              /Users/benjaminmeyer/Downloads/HQHub.jar!/com/primalgamesllc/hqhub/tasks/ServerUpdateTask.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
