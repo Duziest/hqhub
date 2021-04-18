67e84bf686ec11901881c8b4eaa5b67901623190
Duziest <39634650+Duziest@users.noreply.github.com>
2021-04-18 12:17:58 -0400

    Update HQServer.java

--- com/primalgamesllc/hqhub/objects/HQServer.java
+++ com/primalgamesllc/hqhub/objects/HQServer.java
@@ -27,9 +27,3 @@
 /* 27 */     this.serverName = serverName;
 /*    */   }
 /*    */ }
-
-
-/* Location:              /Users/benjaminmeyer/Downloads/HQHub.jar!/com/primalgamesllc/hqhub/objects/HQServer.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
