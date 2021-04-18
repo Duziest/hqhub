457854de049efa6250236eae7f1a8e2b61f8e85f
Duziest <39634650+Duziest@users.noreply.github.com>
2021-04-18 12:22:28 -0400

    Update MenuItem.java

--- com/primalgamesllc/hqhub/menus/MenuItem.java
+++ com/primalgamesllc/hqhub/menus/MenuItem.java
@@ -39,7 +39,3 @@
 /*    */ }
 
 
-/* Location:              /Users/benjaminmeyer/Downloads/HQHub.jar!/com/primalgamesllc/hqhub/menus/MenuItem.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
